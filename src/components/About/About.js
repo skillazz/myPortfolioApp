926b63c27f724508c19b9f461ace30177dc1e839
skillazz <skillaz92@gmail.com>
2022-06-15 00:24:35 +0200

    feat: added floating navigation bar, prepared About section schema.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,27 @@
+import IMAGE from '../../assets/image.jpg'
 
 const About = () => {
     return (
-        <section id="about">About</section>
+        <section id="about">
+            <h5>Let's dive in to the</h5>
+            <h2>About section</h2>
+            <div className="container about__container">
+                <div className="about__wrapper">
+                    <div className="about__pic">
+                        <img src={IMAGE} alt="example photo"/>
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__tiles">
+                        <article className="about__tile">
+                            <h5>Lorem</h5>
+                            <small>Lorem Lorem Lorem*</small>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import './nav.css'
 import {
     AiOutlineCopyrightCircle,
@@ -11,17 +12,20 @@ import {BsAward} from "react-icons/bs";
 
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = useState('#header')
+// TODO
+// fix the state changes on scroll as well.
     return (
         <nav>
             <p>LOGO PLEASE!</p>
             <div className="nav__sidebar">
-                <a href="#header"><AiOutlineHome/></a>
-                <a href="#about"><AiOutlineUser/></a>
-                <a href="#experience"><BsAward/></a>
-                <a href="#portfolio"><AiOutlineProject/></a>
-                <a href="#references"><AiOutlineHeart/></a>
-                <a href="#contact"><AiOutlineMessage/></a>
-                <a href="#footer"><AiOutlineCopyrightCircle/></a>
+                <a href="#header" onClick={() => setActiveNav('#header')}className={activeNav === '#header' ? 'active' : ''}><AiOutlineHome/></a>
+                <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+                <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsAward/></a>
+                <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineProject/></a>
+                <a href="#references" onClick={() => setActiveNav('#references')} className={activeNav === '#references' ? 'active' : ''}><AiOutlineHeart/></a>
+                <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
+                <a href="#footer" onClick={() => setActiveNav('#footer')} className={activeNav === '#footer' ? 'active' : ''}><AiOutlineCopyrightCircle/></a>
             </div>
         </nav>
 
